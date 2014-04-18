d4ffc91b76c858796d0831f1262b6825d847201b
houngj <houngj@students.wwu.edu>
2014-04-18 11:16:05 -0700

    Something went wrong.. just dumping everything to see if it works. Dont judge me tucker

---
+++ tmp/more_utils.py
@@ -0,0 +1,20 @@
+import re
+
+def ProcessFileName(Filename):
+    #Split retrieved filename
+    
+    SFileName = re.sub(r'\..+', "", Filename) 
+    SFileName = SFileName.split("_")
+    #Search through split filename for parameters dur, start, exp 
+    for each in SFileName:
+        if ("dur" in each):
+            Dur = each.replace("dur", "")
+            #print Dur
+        if("start" in each):
+            Start = each.replace("start", "")
+            #print Start
+        if("exp" in each):
+            Exp = each.replace("exp", "")
+            #print Exp
+
+    return [Dur, Start, Exp]
